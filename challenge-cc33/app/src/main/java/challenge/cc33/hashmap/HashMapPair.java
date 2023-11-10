c8ca3281fdca8d128929c466474f9c855dab634f
akmz <balqeesalqudah97@gmail.com>
2023-11-10 21:51:28 +0300

    DONE

---
+++ challenge-cc33/app/src/main/java/challenge/cc33/hashmap/HashMapPair.java
@@ -0,0 +1,15 @@
+package challenge.cc33.hashmap;
+
+import java.util.AbstractMap;
+
+public class HashMapPair<K,V> extends AbstractMap.SimpleEntry<K,V>
+
+{
+    public HashMapPair (K key, V value)
+    {
+        super(key,value);
+    }
+
+
+
+}
