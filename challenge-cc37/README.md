3e4ba280bf533814fc37caf37afefd30ed2f32c2
akmz <balqeesalqudah97@gmail.com>
2023-11-20 23:30:11 +0300

    done

---
+++ challenge-cc37/README.md
@@ -0,0 +1,18 @@
+# graph-business-trip
+
+a function called business trip that returns the cost of the trip (if it’s possible) or null (if not)
+
+## Whiteboard Process
+
+![Whiteboard](app/src/main/resources/cc37WB.jpg)
+
+## Approach & Efficiency
+
+
+- **Time complexity:** is O(n), where n is the length of the trip array, as it iterates through each city in the trip.
+
+- **Space complexity:** is O(1) as the function uses a constant amount of space regardless of the input size
+
+## Solution
+
+![Output](app/src/main/resources/cc37output.jpg)
---
+++ challenge-cc37/app/src/main/java/graph/Edge.java
@@ -0,0 +1,37 @@
+package graph;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Edge {
+    Vertex source;
+    Vertex destination;
+    int weight;
+
+    public Edge(Vertex source, Vertex destination, int weight) {
+        this.source = source;
+        this.destination = destination;
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Edge{" +
+                "source=" + source +
+                ", destination=" + destination +
+                ", weight=" + weight +
+                '}';
+    }
+
+    public Vertex getSource() {
+        return source;
+    }
+
+    public Vertex getDestination() {
+        return destination;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+}
